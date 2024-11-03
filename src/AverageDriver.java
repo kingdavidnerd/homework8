9abb2d22ce4fe3cd91644b33008eca4f1573a830
kingdavidnerd <mrdavidworks@gmail.com>
2024-11-03 11:54:10 -0500

    FInished HW 8

---
+++ src/AverageDriver.java
@@ -0,0 +1,9 @@
+public class AverageDriver {
+    public static void main(String[] args) {
+        Average average = new Average();
+
+        System.out.println(average.toString());
+        // System.out.println(average);
+
+    }
+}
